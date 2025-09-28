87e0f1d5c132cadbe4062ba1afdf5adb10b65f12
Chris Petersen <chris@petersen.io>
2025-09-28 06:18:51 -0700

    Fixed a compiler warning

--- ext/tokenkit/src/config.rs
+++ ext/tokenkit/src/config.rs
@@ -25,13 +25,3 @@ impl Default for TokenizerConfig {
         }
     }
 }
-
-impl TokenizerConfig {
-    pub fn to_json(&self) -> Result<String, serde_json::Error> {
-        serde_json::to_string(self)
-    }
-
-    pub fn from_json(json: &str) -> Result<Self, serde_json::Error> {
-        serde_json::from_str(json)
-    }
-}
