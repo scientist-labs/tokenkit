16dc1181e1ab06c1222076a7e0ada68bba0827b3
Chris Petersen <chris@petersen.io>
2025-09-27 19:33:30 -0700

    Intial commit

---
+++ lib/tokenkit.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tokenkit/version"
+
+module Tokenkit
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tokenkit/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Tokenkit
+  VERSION = "0.1.0"
+end
